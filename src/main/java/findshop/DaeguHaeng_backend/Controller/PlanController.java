b4330543988dbf0d8e880eb149bb4dad937e6df7
minseok <minsuk6803@gmail.com>
2024-11-18 02:50:28 +0900

    PlanController 구현 완료

--- src/main/java/findshop/DaeguHaeng_backend/Controller/PlanController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/PlanController.java
@@ -1,22 +1,58 @@
 package findshop.DaeguHaeng_backend.Controller;
 
+import findshop.DaeguHaeng_backend.DTO.PlanRequestDTO;
+import findshop.DaeguHaeng_backend.DTO.PlanResponseDTO;
 import findshop.DaeguHaeng_backend.Service.PlanService;
 import findshop.DaeguHaeng_backend.Service.ScheduleService;
 import findshop.DaeguHaeng_backend.Service.UserService;
+import findshop.DaeguHaeng_backend.domain.Plan;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/plans")
+@RequiredArgsConstructor
 public class PlanController {
-    private final UserService userService;
     private final PlanService planService;
-    private final ScheduleService scheduleService;
-    @Autowired
-    public PlanController(UserService userService, PlanService planService, ScheduleService scheduleService) {
-        this.userService = userService;
-        this.planService = planService;
-        this.scheduleService = scheduleService;
+
+    @PostMapping("/api/recomands/{id}")
+    public ResponseEntity<?> createPlan(@PathVariable Long id, @RequestBody PlanRequestDTO planRequestDTO) {
+        PlanResponseDTO planResponseDTO;
+        try {
+            planResponseDTO = planService.initPlan(id, planRequestDTO);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(planResponseDTO, HttpStatus.CREATED);
     }
+    @GetMapping("/api/{id}/plans")
+    public ResponseEntity<?> getAllPlans(@PathVariable Long id) {
+        List<PlanResponseDTO> plans = planService.findPlans(id);
+        return new ResponseEntity<>(plans,HttpStatus.OK);
+    }
+
+    @PutMapping("/api/{id}/plans")
+    public ResponseEntity<Plan> updatePlan(@PathVariable Long id, @RequestBody PlanRequestDTO planRequestDTO) {
+        try{ planService.updatePlan(id,planRequestDTO);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api/{id}/plans")
+    public ResponseEntity<String> deletePlan(@PathVariable Long id) {
+        try{
+            planService.deletePlan(id);
+        }catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
 }
