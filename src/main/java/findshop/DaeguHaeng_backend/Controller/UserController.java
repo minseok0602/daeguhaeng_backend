2bb3b06c826d4a75d90faf42472da4d39c7458ae
minseok <minsuk6803@gmail.com>
2024-11-18 02:50:56 +0900

    UserController RequiredArgsConstructor 애노테이션 부착

--- src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
@@ -5,20 +5,17 @@ import findshop.DaeguHaeng_backend.DTO.RegisterRequestDTO;
 import findshop.DaeguHaeng_backend.DTO.UpdatePasswordDTO;
 import findshop.DaeguHaeng_backend.DTO.UserDTO;
 import findshop.DaeguHaeng_backend.Service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
         try {
