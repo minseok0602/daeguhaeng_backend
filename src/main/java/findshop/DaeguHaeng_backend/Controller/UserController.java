ac12d59f50d106997771b0d2e73c5ef7455a0326
minseok <minsuk6803@gmail.com>
2024-11-18 00:21:31 +0900

    회원가입 기능 구현

--- src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
@@ -1,4 +1,28 @@
 package findshop.DaeguHaeng_backend.Controller;
 
+import findshop.DaeguHaeng_backend.DTO.RegisterRequestDTO;
+import findshop.DaeguHaeng_backend.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    public ResponseEntity<String> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
+        try {
+            userService.register(registerRequestDTO);
+            return new ResponseEntity<>("User registered successfully!", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>("User registration failed!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
