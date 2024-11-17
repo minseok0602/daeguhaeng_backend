b73ad36f45e0d0f0802b5e8a612957cc47c904f3
minseok <minsuk6803@gmail.com>
2024-11-18 01:11:25 +0900

    비밀번호 수정 http 경로 수정

--- src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
@@ -2,6 +2,8 @@ package findshop.DaeguHaeng_backend.Controller;
 
 import findshop.DaeguHaeng_backend.DTO.LoginRequestDTO;
 import findshop.DaeguHaeng_backend.DTO.RegisterRequestDTO;
+import findshop.DaeguHaeng_backend.DTO.UpdatePasswordDTO;
+import findshop.DaeguHaeng_backend.DTO.UserDTO;
 import findshop.DaeguHaeng_backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -35,5 +37,25 @@ public class UserController {
             return new ResponseEntity<>("User login failed!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    //회원 정보 수정은 어캐함?
+
+
+    @PutMapping("/{userId}/name")
+    public ResponseEntity<String> updateUserName(@PathVariable Long userId, @RequestBody String newName) {
+        try{
+            UserDTO isUpdated = userService.modifyUserName(userId,newName);
+        }catch (Exception e) {
+            return new ResponseEntity<>("User update failed!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("User name updated successfully!", HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}/settings")
+    public ResponseEntity<String> updatePassword(@PathVariable Long userId, @RequestBody UpdatePasswordDTO updatePasswordDTO) {
+        try{
+            UserDTO isUpdated = userService.modifyPassword(userId, updatePasswordDTO);
+        }catch (Exception e) {
+            return new ResponseEntity<>("User update failed!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>("User password updated successfully!", HttpStatus.OK);
+    }
 }
