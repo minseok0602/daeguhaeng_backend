cb8395e323bc959a0c23cdee19e79678885703bf
minseok <minsuk6803@gmail.com>
2024-11-18 00:25:41 +0900

    로그인 기능 구현

--- src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/UserController.java
@@ -1,10 +1,12 @@
 package findshop.DaeguHaeng_backend.Controller;
 
+import findshop.DaeguHaeng_backend.DTO.LoginRequestDTO;
 import findshop.DaeguHaeng_backend.DTO.RegisterRequestDTO;
 import findshop.DaeguHaeng_backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +19,23 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
         try {
             userService.register(registerRequestDTO);
             return new ResponseEntity<>("User registered successfully!", HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>("User registration failed!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        try{
+            userService.logIn(loginRequestDTO);
+            return new ResponseEntity<>("User logged in successfully!", HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>("User login failed!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
