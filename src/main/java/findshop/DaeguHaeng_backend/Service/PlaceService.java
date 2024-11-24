1ce419022dd0a27e7a17639cf7d4b6e20dcf194a
minseok <minsuk6803@gmail.com>
2024-11-24 21:11:34 +0900

    Place 생성 로직 구현

--- src/main/java/findshop/DaeguHaeng_backend/Service/PlaceService.java
+++ src/main/java/findshop/DaeguHaeng_backend/Service/PlaceService.java
@@ -1,12 +1,17 @@
 package findshop.DaeguHaeng_backend.Service;
 
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import findshop.DaeguHaeng_backend.DTO.PlaceDTO;
 import findshop.DaeguHaeng_backend.Repository.PlaceRepository;
-import findshop.DaeguHaeng_backend.domain.Place;
+import findshop.DaeguHaeng_backend.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,11 +19,23 @@ public class PlaceService {
 
     @Autowired
     private final PlaceRepository placeRepository;
-
-    public PlaceDTO findPlace(Long placeId){
+    public Place findPlace(String placeJson,Long placeId) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonParser parser = objectMapper.createParser(placeJson);
+        JsonNode node = objectMapper.readTree(parser);
         Place place = placeRepository.findById(placeId);
-        if(place == null) throw new IllegalStateException("존재하지 않는 Place");
-
-        return place.placeDTO();
+        if(place == null) {
+            String dtype = node.get("dtype").asText();
+            if(dtype.equals("Hotel")) {
+                place = objectMapper.treeToValue(node, Hotel.class);
+            }
+            else if(dtype.equals("Attraction")) {
+                place = objectMapper.treeToValue(node, Attraction.class);
+            }
+            else if(dtype.equals("Restaurant")) {
+                place = objectMapper.treeToValue(node, Restaurant.class);
+            }
+        }
+        return place;
     }
 }
