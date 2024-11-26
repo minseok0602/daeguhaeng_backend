57915cc81ebd1f644e212c9efc18160b21b81205
brothergiven <dung3740@gmail.com>
2024-11-26 20:07:44 +0900

    Place 생성자 수정
    Place 생성자 수정

--- src/main/java/findshop/DaeguHaeng_backend/domain/Hotel.java
+++ src/main/java/findshop/DaeguHaeng_backend/domain/Hotel.java
@@ -3,19 +3,17 @@ package findshop.DaeguHaeng_backend.domain;
 import findshop.DaeguHaeng_backend.DTO.PlaceDTO;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @DiscriminatorValue("H")
 @Getter @Setter
+@RequiredArgsConstructor
 public class Hotel extends Place {
-    private PlaceDTO placeDTO;
-    public Hotel(PlaceDTO placeDTO) {
-        this.placeDTO = placeDTO;
-    }
-
-    public Hotel() {
-
+    public Hotel(PlaceDTO placeDTO){
+        super(placeDTO);
     }
 }
--- src/main/java/findshop/DaeguHaeng_backend/domain/Restaurant.java
+++ src/main/java/findshop/DaeguHaeng_backend/domain/Restaurant.java
@@ -3,19 +3,17 @@ package findshop.DaeguHaeng_backend.domain;
 import findshop.DaeguHaeng_backend.DTO.PlaceDTO;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
 @DiscriminatorValue("R")
+@RequiredArgsConstructor
 public class Restaurant extends Place {
-    private PlaceDTO placeDTO;
-    public Restaurant(PlaceDTO placeDTO) {
-        this.placeDTO = placeDTO;
-    }
-
-    public Restaurant() {
-
+    public Restaurant(PlaceDTO placeDTO){
+        super(placeDTO);
     }
 }
