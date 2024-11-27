b95eef033e60361064ee2598d071ec8f7f4bda84
minseok <minsuk6803@gmail.com>
2024-11-28 00:39:33 +0900

    API 연결 설정

--- src/main/java/findshop/DaeguHaeng_backend/DTO/ClientDTO.java
+++ src/main/java/findshop/DaeguHaeng_backend/DTO/ClientDTO.java
@@ -3,11 +3,56 @@ package findshop.DaeguHaeng_backend.DTO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 public class ClientDTO {
+    private int cluster;
     private int age;
     private String gender;
-    private int spending;
-    private int clusterId;
+    private Map<String, Double> spending; // 금액 항목별로 저장
+
+    // Getters and Setters
+    public int getCluster() {
+        return cluster;
+    }
+
+    public void setCluster(int cluster) {
+        this.cluster = cluster;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Map<String, Double> getSpending() {
+        return spending;
+    }
+
+    public void setSpending(Map<String, Double> spending) {
+        this.spending = spending;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientInfo{" +
+                "cluster=" + cluster +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", spending=" + spending +
+                '}';
+    }
 }
--- src/main/java/findshop/DaeguHaeng_backend/Service/APIService.java
+++ src/main/java/findshop/DaeguHaeng_backend/Service/APIService.java
@@ -1,15 +1,30 @@
 package findshop.DaeguHaeng_backend.Service;
 
 import findshop.DaeguHaeng_backend.DTO.ClientDTO;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class APIService {
-    private final String FLASK_API_URL = "http://127.0.0.1:5000/recommend";
+    private final String FLASK_API_URL = "http://127.0.0.1:5000/api/recommend";
 
-    public Object callFlaskAPI(ClientDTO clientInfo) {
+    public Object callFlaskAPI(ClientDTO clientDTO) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.postForObject(FLASK_API_URL, clientInfo, Object.class);
+
+        // HTTP 요청 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+
+        // 요청 본문(body) 생성
+        HttpEntity<ClientDTO> request = new HttpEntity<>(clientDTO, headers);
+
+        // Flask 서버로 POST 요청 보내기
+        ResponseEntity<String> response = restTemplate.postForEntity(FLASK_API_URL, request, String.class);
+
+        // 응답 반환
+        return response.getBody();
     }
 }
