31395138316fbf52d5bccfdb27a09aae3fa4e1fb
brothergiven <dung3740@gmail.com>
2024-12-03 21:38:23 +0900

    Cors 설정 모든 origin에서 되도록
    Cors 설정 모든 origin에서 되도록

--- src/main/java/findshop/DaeguHaeng_backend/Controller/HomeController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:5173/") // 클라이언트 Origin 허용
+@CrossOrigin(origins = "http://localhost:*") // 클라이언트 Origin 허용
 public class HomeController {
 
     @RequestMapping("/")
