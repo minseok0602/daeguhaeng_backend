b0859a891bfe5c8a0878e68cee5338108d89f5f5
brothergiven <dung3740@gmail.com>
2024-12-03 22:04:10 +0900

    Revert "CORS 모든 origin에서 허용되도록"
    This reverts commit 4609c087d377596881184334887ce450c587adbd.

--- src/main/java/findshop/DaeguHaeng_backend/Controller/PlaceController.java
+++ src/main/java/findshop/DaeguHaeng_backend/Controller/PlaceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController("/api/place")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*") // 클라이언트 Origin 허용
+@CrossOrigin(origins = "http://localhost:5173/") // 클라이언트 Origin 허용
 public class PlaceController {
     final PlaceService placeService;
     /*@GetMapping("/{placeId}")
