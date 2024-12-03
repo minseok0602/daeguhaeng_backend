3a56c50dd136401946eec43f7ad0b594f6701ad6
brothergiven <dung3740@gmail.com>
2024-12-03 21:30:34 +0900

    CORS 모든 Origin에서 허용되도록
    CORS 모든 Origin에서 허용되도록

--- src/main/java/findshop/DaeguHaeng_backend/Config/WebConfig.java
+++ src/main/java/findshop/DaeguHaeng_backend/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
